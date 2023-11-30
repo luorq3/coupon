de3162b4aa0f8d220054de1daa4ae4362faf4c9a
luorq3 <ruiqingluo1@gmail.com>
2023-11-30 15:26:56 +0800

    OpenFeign

--- coupon/coupon-customer-serv/coupon-customer-impl/src/main/java/com/geekbang/coupon/customer/Configuration.java
+++ coupon/coupon-customer-serv/coupon-customer-impl/src/main/java/com/geekbang/coupon/customer/Configuration.java
@@ -1,5 +1,6 @@
 package com.geekbang.coupon.customer;
 
+import feign.Logger;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -11,5 +12,10 @@ public class Configuration {
     @LoadBalanced
     public WebClient.Builder register() {
         return WebClient.builder();
+    }
+
+    @Bean
+    Logger.Level feignLogger() {
+        return Logger.Level.FULL;
     }
 }
